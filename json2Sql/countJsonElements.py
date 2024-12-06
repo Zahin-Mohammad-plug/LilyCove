c03837b9a6599792c063e7b8560d267c5247665a
Zahin Mohammad <74513580+Zahin-Mohammad-plug@users.noreply.github.com>
2024-12-05 20:55:08 -0500

    Tested API's for pricing data and more tools

--- json2Sql/countJsonElements.py
+++ json2Sql/countJsonElements.py
@@ -10,6 +10,32 @@ def count_cards(database_path):
         # Connect to the database
         conn = sqlite3.connect(database_path)
         cursor = conn.cursor()
+        
+        # Query total Series
+        cursor.execute("SELECT COUNT(*) FROM series;")
+        total_series = cursor.fetchone()[0]
+
+        # Query distinct Series
+        cursor.execute("SELECT COUNT(DISTINCT id) FROM series;")
+        distinct_series = cursor.fetchone()[0]
+
+        # Print the results
+        print(f"Total Series: {total_series}")
+        print(f"Distinct Series: {distinct_series}")
+        print("\n")
+        
+        # Query total Sets
+        cursor.execute("SELECT COUNT(*) FROM sets;")
+        total_sets = cursor.fetchone()[0]
+
+        # Query distinct Sets
+        cursor.execute("SELECT COUNT(DISTINCT id) FROM sets;")
+        distinct_sets = cursor.fetchone()[0]
+
+        # Print the results
+        print(f"Total Sets: {total_sets}")
+        print(f"Distinct Sets: {distinct_sets}")
+        print("\n")
 
         # Query total cards
         cursor.execute("SELECT COUNT(*) FROM cards;")
@@ -22,6 +48,7 @@ def count_cards(database_path):
         # Print the results
         print(f"Total Cards: {total_cards}")
         print(f"Distinct Cards: {distinct_cards}")
+        
 
     except sqlite3.Error as e:
         print(f"Database error: {e}")
